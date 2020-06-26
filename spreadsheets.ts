6e92d153e7c8490a73b3054f5500dacbf96400d4
hhimanshu <harit.himanshu@yahoo.in>
2020-06-25 17:55:46 -0700

    Adding a transaction

--- spreadsheets.ts
+++ spreadsheets.ts
@@ -7,10 +7,22 @@ const createSheet = () => {
     activeSheet.setName(sheetName)
 }
 
+type Transaction = {
+    name: string,
+    amount: number,
+    when: Date,
+}
 const main = () => {
     const file = DriveApp.getFilesByName(spreadSheetName).next();
     const spreadsheet = SpreadsheetApp.open(file);
     const sheet = spreadsheet.getSheetByName(sheetName);
     spreadsheet.setActiveSheet(sheet);
     console.log(`${spreadsheet.getUrl()}`)
+
+    const addTransaction = (t: Transaction) => {
+        // Adding a row
+        sheet.appendRow([t.name, t.amount, t.when])
+    }
+    addTransaction({name: "Drip Coffee", amount: 10.22, when: new Date()})
+
 }
